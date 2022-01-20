aa152227fa051f57bb87209fe4f2ab61553645ea
Anandakumar Palanisamy <ananda11k@gmail.com>
2022-01-20 18:23:35 +0000

    Angular routing example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
+import { FirstComponent } from './first/first.component';
+import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
+import { SecondComponent } from './second/second.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'first-component', component: FirstComponent,
+    children: [
+      {
+        path: 'child-a',
+        component: ChildAComponent
+      },
+      {
+        path: 'child-b',
+        component: ChildBComponent
+      }
+    ]
+  },
+  {
+    path: 'second-component', component: SecondComponent
+  },
+  {
+    path: '', redirectTo: '/first-component', pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PathNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    PathNotFoundComponent,
+    ChildAComponent,
+    ChildBComponent
   ],
   imports: [
     BrowserModule,
